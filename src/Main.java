b64afb99c2e9da31453dbafc71d5b9ac10093329
Couper J Corbett 11th CCHS <102764@SWCSD.US>
2024-01-24 13:32:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int lowTemp = 32;
+        String days[] = {"sunday", "monday","tuesday","wednesday","thursday","friday","saturday"};
+        int dailyHighs[] = {45, 29, 10, 22, 41, 33};
+        int precip[] = {95, 60, 25, 5, 0, 75, 90};
+
+        for(int i = 0; i < dailyHighs.length; i++) {
+            if(dailyHighs[i]<=lowTemp) {
+                if (precip[i] >= 50) {
+                    System.out.print("it will snow on " + days[i]);
+                } else {
+                    System.out.print("it will be cold but won't snow on " + days[i]);
+                }
+            }
+            else if(dailyHighs[i]>lowTemp){
+                if (precip[i] >= 50) {
+                    System.out.print("it will be warmer and rain on " + days[i]);
+                } else {
+                    System.out.print("it will be warmer and wont rain on " + days[i]);
+                }
+            }
+            System.out.println(" | " + dailyHighs[i] + " degrees | " + precip[i] + "% chance of precipitation");
+        }
+        }
+    }
